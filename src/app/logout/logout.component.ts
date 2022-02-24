8ae211861bf38ce0d8c2e16274eeae43113a2aec
Manuel Zavatta <manuel.zavatta@gmail.com>
2022-02-24 16:09:12 +0100

    Logout component

---
+++ src/app/logout/logout.component.ts
@@ -0,0 +1,25 @@
+import {Component,OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {SessionService} from "../../services/session/session.service";
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent implements OnInit{
+
+  constructor(
+		private router:Router,
+		private sessionService: SessionService,
+	)
+	{
+		this.sessionService.logout();
+		this.router.navigateByUrl('/login');
+	}
+
+  ngOnInit():void{
+		console.log('logout component init');
+  }
+
+}
